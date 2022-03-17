6f7b8a17872175228aa1800fe86f3cb01305ef27
OggyP <oggyp@oggyp.com>
2022-03-18 10:44:12 +1100

    working!

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ clock = pygame.time.Clock()
 
 sim_running = True
 
-wait = 5
+key_pressed = True
+
+tick = 0
 
 while sim_running:
     clock.tick(MAX_MOVEMENT_SPEED / SEAT_DIMENSIONS[0])
@@ -22,12 +24,15 @@ while sim_running:
             sim_running = True
             pygame.quit()
             exit()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            key_pressed = True
 
+    # if key_pressed:
     WINDOW.fill((0, 0, 0))
     plane.draw_seats(WINDOW)
     plane.draw_and_update_passengers(WINDOW)
-    if wait == 0:
-        wait = 6
-        plane.attempt_to_create_passenger()
+    plane.attempt_to_create_passenger()
     pygame.display.update()
-    wait -= 1
+    # pygame.image.save(WINDOW, "./frames/frame_" + str(tick) + ".jpeg")
+    tick += 1
+    key_pressed = False
--- passengers.py
+++ passengers.py
@@ -31,7 +31,8 @@ class Passenger():
             HEIGHT - ((self.ending_seat.x + 2)
                       * SEAT_DIMENSIONS[0] * SCALE - MIN_SEAT_DIM_SCALED),
         )
-        pygame.draw.line(WINDOW, self.colour, person_circle_centre, person_line_end, 2)
+        pygame.draw.line(WINDOW, self.colour,
+                         person_circle_centre, person_line_end, 2)
 
     def update(self, passengers: list[list[any]]):
         if self.skip:
@@ -56,14 +57,17 @@ class Passenger():
                     return None
             else:
                 if difference_in_pos.y == 1:
-                    check_for_people_pos = Vector2(
-                        self.pos.x, self.ending_seat.y)
                     direction_to_seat_x = int(
                         difference_in_pos.x / abs(difference_in_pos.x))
+                    check_for_people_pos = Vector2(
+                        self.pos.x + direction_to_seat_x, self.ending_seat.y)
 
                     detected_people: list[Vector2] = []
                     while (direction_to_seat_x > 0 and check_for_people_pos.x < self.ending_seat.x) or (direction_to_seat_x < 0 and check_for_people_pos.x > self.ending_seat.x):
-                        if get_passengers(check_for_people_pos) is not None:
+                        possible_passenger = get_passengers(check_for_people_pos)
+                        if possible_passenger is not None:
+                            if possible_passenger.at_seat == False:
+                                return None
                             detected_people.append(
                                 Vector2(check_for_people_pos))
                         check_for_people_pos.x += direction_to_seat_x
@@ -79,15 +83,16 @@ class Passenger():
                             return None
                         self.waiting_for_seat_shuffle = True
                         return detected_people_updates
-                for i in range(1, 4):
-                    check_pos = Vector2(self.pos)
-                    check_pos.y += i
-                    check_passenger = get_passengers(check_pos)
-                    if check_passenger is not None and not check_passenger.skip and check_passenger.ending_seat.y < check_passenger.pos.y:
-                        if check_passenger.ending_seat.y == self.ending_seat.y:
-                            continue
-                        else:
-                            return None
+                if not self.ending_seat.y < self.pos.y:
+                    for i in range(4):
+                        check_pos = Vector2(self.pos)
+                        check_pos.y += i
+                        check_passenger = get_passengers(check_pos)
+                        if check_passenger is not None and check_passenger.ending_seat.y < check_passenger.pos.y:
+                            if check_passenger.ending_seat.y == self.ending_seat.y and (difference_in_pos.y == 1 or difference_in_pos.y == 0) :
+                                continue
+                            else:
+                                return None
 
                 direction_to_seat = int(
                     difference_in_pos.y / abs(difference_in_pos.y))
@@ -107,7 +112,7 @@ class Passenger():
                     difference_in_pos.y / abs(difference_in_pos.y))
                 new_pos = Vector2(self.pos)
                 new_pos.y += direction_to_seat
-                if get_passengers(new_pos) == None:
+                if get_passengers(new_pos) == None and get_passengers(self.go_to) == None:
                     self.pos = new_pos
                     return None
             else:
@@ -115,7 +120,7 @@ class Passenger():
                     difference_in_pos.x / abs(difference_in_pos.x))
                 new_pos = Vector2(self.pos)
                 new_pos.x += direction_to_seat
-                if get_passengers(new_pos) == None:
+                if get_passengers(new_pos) == None and get_passengers(self.go_to) == None:
                     self.pos = new_pos
                     return None
             return None
